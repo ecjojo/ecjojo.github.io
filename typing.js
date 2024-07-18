a06ed45dac24c04ada79f0244defe2bb79936285
ecjojo <48451938+ecjojo@users.noreply.github.com>
2024-07-18 22:48:02 +0800

    link and some style updated

--- typing.js
+++ typing.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".typed").typed({
-        strings: ["a Developer.", "AIGC Explorer.", "I make Website.", "I make AI Chatbot.", "I make ComfyUI Workflow."],
+        strings: ["a Developer.", "a Creator.", "I make ComfyUI Workflow.","I'm building creative project with AIGen."],
         // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
         stringsElement: null,
         // typing speed
